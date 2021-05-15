ee7efd83b9910ca24b10fc6babfa87911235bc19
Scott Brenner <brenner.scott.980@icloud.com>
2021-05-14 20:25:55 -0700

    SAB

--- Sources/SBVapor/Crypto/EncryptedContent.swift
+++ Sources/SBVapor/Crypto/EncryptedContent.swift
@@ -2,22 +2,25 @@ import Vapor
 
 public struct EncryptedContent: Content {
     
-    /// Encrypted data that has been Base-64 encoded.
-    public let encrypted: String
+    public let id: String
     
-    fileprivate init(encrypted: Encryption.Encrypted) {
-        self.encrypted = encrypted.base64EncodedString
+    /// Encrypted data that has been Base-64 encoded.
+    public let value: String
+    
+    fileprivate init(id: String, value: Encryption.Encrypted) {
+        self.id = id
+        self.value = value.base64EncodedString
     }
 }
 
 public extension EncryptedContent {
     
     func decrypted<T: Content>(
-        as value: T.Type,
+        as type: T.Type,
         with key: SymmetricKey,
         decoder: JSONDecoder = .init()
     ) throws -> T {
-        let data = try Encryption.decrypt(encrypted, with: key).get()
+        let data = try Encryption.decrypt(self.value, with: key).get()
         return try decoder.decode(T.self, from: data)
     }
 }
@@ -25,12 +28,13 @@ public extension EncryptedContent {
 public extension Content {
     
     func encrypted(
-        with key: SymmetricKey,
+        with id: String,
+        key: SymmetricKey,
         encoder: JSONEncoder = .init()
     ) throws -> EncryptedContent {
         let data = try encoder.encode(self)
         let encrypted = try Encryption.encrypt(data, with: key).get()
-        return .init(encrypted: encrypted)
+        return .init(id: id, value: encrypted)
     }
 }
 
@@ -49,9 +53,10 @@ public extension ContentContainer {
 public extension EventLoopFuture where Value: Content {
     
     func encrypted(
-        with key: SymmetricKey,
+        with id: String,
+        key: SymmetricKey,
         encoder: JSONEncoder = .init()
     ) -> EventLoopFuture<EncryptedContent> {
-        flatMapThrowing { try $0.encrypted(with: key, encoder: encoder) }
+        flatMapThrowing { try $0.encrypted(with: id, key: key, encoder: encoder) }
     }
 }
--- Tests/SBVaporTests/EncryptedContentTests.swift
+++ Tests/SBVaporTests/EncryptedContentTests.swift
@@ -4,11 +4,13 @@ import XCTest
 final class EncryptedContentTests: XCTestCase {
     
     func testEncryptionAndDecryption() {
+        let mockID = "mock"
         let content = TestContent(date: .init())
         let key = SymmetricKey(size: .bits256)
         do {
-            let encrypted = try content.encrypted(with: key)
+            let encrypted = try content.encrypted(with: mockID, key: key)
             let decrypted = try encrypted.decrypted(as: TestContent.self, with: key)
+            XCTAssertEqual(encrypted.id, mockID)
             XCTAssertEqual(content.date, decrypted.date)
             let wrongKey = SymmetricKey(size: .bits256)
             XCTAssertThrowsError(try encrypted.decrypted(as: TestContent.self, with: wrongKey))
