56e667f40477bd4462613adbfecd1ce0a23e4fac
Scott Brenner <brenner.scott.980@icloud.com>
2021-05-17 15:44:12 -0700

    SAB

--- Sources/SBVapor/UserDevice.swift
+++ Sources/SBVapor/UserDevice.swift
@@ -1,16 +1,6 @@
 import Vapor
 
-public struct UserDevice: Content, Validatable {
-    
-    enum CodingKeys: String, CodingKey {
-        case applicationVersion = "application_version",
-             name,
-             systemName = "system_name",
-             systemVersion = "system_version",
-             model,
-             identifierForVendor = "identifier_for_vendor",
-             publicKey = "public_key"
-    }
+public struct UserDevice: Content {
     
     /// The version of the iOS application installed on the device.
     public let applicationVersion: String
@@ -29,17 +19,4 @@ public struct UserDevice: Content, Validatable {
     
     /// An alphanumeric string that uniquely identifies a device to the app’s vendor.
     public let identifierForVendor: String
-    
-    /// The user's public key x963 representation data Base-64 encoded.
-    public let publicKey: String
-    
-    public static func validations(_ validations: inout Validations) {
-        validations.add(CodingKeys.applicationVersion, as: String.self, is: !.empty)
-        validations.add(CodingKeys.name, as: String.self, is: !.empty)
-        validations.add(CodingKeys.systemName, as: String.self, is: !.empty)
-        validations.add(CodingKeys.systemVersion, as: String.self, is: !.empty)
-        validations.add(CodingKeys.model, as: String.self, is: !.empty)
-        validations.add(CodingKeys.identifierForVendor, as: String.self, is: !.empty)
-        validations.add(CodingKeys.publicKey, as: String.self, is: !.empty)
-    }
 }
