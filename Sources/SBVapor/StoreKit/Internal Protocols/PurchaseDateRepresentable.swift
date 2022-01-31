a3087af25d5e1b98d720dd5e5439a59369d7ae24
Scott Brenner <brenner.scott.980@icloud.com>
2022-01-31 13:15:43 -0700

    SAB

--- Sources/SBVapor/StoreKit/Internal Protocols/PurchaseDateRepresentable.swift
+++
@@ -1,8 +0,0 @@
-
-protocol PurchaseDateRepresentable {
-    
-    /**
-     For consumable, non-consumable, and non-renewing subscription products, the time the App Store charged the user's account for a purchased or restored product. For auto-renewable subscriptions, the time the App Store charged the user’s account for a subscription purchase or renewal after a lapse.
-     */
-    var purchaseDate: Date? { get }
-}
--- Sources/SBVapor/StoreKit/Internal Protocols/WebOrderLineItemIdentifiable.swift
+++
@@ -1,8 +0,0 @@
-
-protocol WebOrderLineItemIdentifiable {
-    
-    /**
-     A unique identifier for purchase events across devices, including subscription-renewal events. This value is the primary key for identifying subscription purchases.
-     */
-    var webOrderLineItemID: String? { get }
-}
